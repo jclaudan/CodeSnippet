7305acaeef4966cabd90d3517264d3c25a4ba21d
spectre <e.sahinnn61@gmail.com>
2025-01-29 16:55:38 +0100

    fix: link

--- Frontend/src/app/page.jsx
+++ Frontend/src/app/page.jsx
@@ -30,7 +30,7 @@ const HomePage = () => {
     } else {
       const fetchSnippets = async () => {
         const response = await fetch(
-          `https://codesnippet-bffb.onrender.com/snippets/user/${userId}`,
+          `https://codesnippet-cy4q.onrender.com/snippets/user/${userId}`,
           {
             method: "GET",
             headers: {
