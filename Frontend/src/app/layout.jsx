8624b50fbb889ded7f70e147552d5862efc9a18c
spectre <e.sahinnn61@gmail.com>
2025-01-29 14:41:34 +0100

    feat: vercel analytics

--- Frontend/src/app/layout.jsx
+++ Frontend/src/app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Code Snippet",
@@ -8,6 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <Analytics />
       <body>{children}</body>
     </html>
   );
