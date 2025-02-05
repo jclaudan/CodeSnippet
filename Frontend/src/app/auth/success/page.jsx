be0349054f81c4c689f4606f2b867f7b085ac832
spectre <e.sahinnn61@gmail.com>
2025-02-05 16:22:44 +0100

    style: success page

--- Frontend/src/app/auth/success/page.jsx
+++ Frontend/src/app/auth/success/page.jsx
@@ -41,12 +41,12 @@ function AuthSuccessContent() {
   }, []);
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-zinc-900">
-      <div className="bg-zinc-800 p-8 rounded-lg shadow-md text-center max-w-md w-full mx-4">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-zinc-900 p-8 rounded-lg shadow-md text-center max-w-md w-full mx-4">
         <h2 className="text-2xl font-bold mb-6">Authentication successful !</h2>
         <div className="flex flex-col items-center gap-4">
-          <div className="animate-spin rounded-full h-10 w-10 border-4 border-zinc-700 border-t-indigo-600"></div>
-          <p className="text-gray-100">Redirecting...</p>
+          <div className="animate-spin rounded-full h-10 w-10 border-4 border-gray-200 border-t-indigo-600"></div>
+          <p className="text-gray-600">Redirecting...</p>
         </div>
       </div>
     </div>
